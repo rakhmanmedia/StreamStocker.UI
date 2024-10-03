8cc2794668bd5c9cd7f60c4ef9e84f1d09e6dbb2
Sergei Rakhman <sergeirakhman@outlook.com>
2024-10-03 10:11:29 +0300

    Создание компонета и сервиса auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
